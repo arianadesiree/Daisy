6950b37af21a1fd551185cdccbdd013df8ac1321
Ariana Jackson <ariana.desiree@gmail.com>
2023-04-11 18:38:29 -0500

    search bar beginning to set up

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,13 +1,18 @@
-
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Home () {
 
-    return (
-        <div>
+ 
+        return (
+            <div className="search-container">
+                        
+            <input type="text" className="search-bar" placeholder="Type Specialty Here" name='searchBar'
+            />
+            <button type='button' className="searchbtn"> Search </button>
 
+            </div>
+        )
 
-        <h1> Home </h1>
-
-        </div>
-    )
 }
